e6419aa9cf9c28961d5d794e3f3ba71699d836e1
Sahil Rajput <sahilrajput03@gmail.com>
2023-01-13 07:05:00 +0530

    Add suggestions and fixes.

--- Api-fetch/src/Copys/Coffeesforin.tsx
+++ Api-fetch/src/Copys/Coffeesforin.tsx
@@ -1,20 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import { getCoffeesData } from '../requests/coffeeRequests';
 import Coffee from '../components/Coffee';
+import { coffeesType } from '../types/CoffeeTypes';
 
 const Coffeesforin = () => {
-  const [coffeeData, setCoffeeData] = useState([]);
+  // NOTE: We could have type as `any` instead of `coffeesType` but with a given type we get autocomplete and type error facility thanks to typescript.
+  const [coffeeData, setCoffeeData] = useState<coffeesType>([]);
 
   useEffect(() => {
     async function main() {
       const coffees = await getCoffeesData();
-      let coffeesNumbered = coffees;
-      coffeesNumbered = [];
+      // NOTE: We could have type as `any` instead of `coffeesType` but with a given type we get autocomplete and type error facility thanks to typescript.
+      let coffeesNumbered: coffeesType = [];
       for (let index in coffees) {
         let item: any = coffees[index];
-        coffeesNumbered.push({ ...item, title: `${index}.${item.title}` });
+        // NOTE: We're converting index to number type becoz index is string type and we dont' want to
+        // concatenate 1 to it. Instead we want to add 1 to it becoz index starts at 0 and we want our
+        // serial to start from 1 and not 0.
+        const serial = Number(index) + 1
+        coffeesNumbered.push({ ...item, title: `${serial}.${item.title}` });
       }
-      console.log(coffeesNumbered);
       setCoffeeData(coffeesNumbered);
     }
     main();
